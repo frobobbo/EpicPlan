9c6557bd6368fa93d9db82834a7aa13bc828cd24
Brett Johnson <johnsobd@gmail.com>
2021-10-07 10:28:54 -0400

    firt commit

---
+++ app/Language/english/custom_lang.php
@@ -0,0 +1,22 @@
+<?php //copy from default_lang.php file and update
+
+$lang["example"] = "Example";
+$lang["websites"] = "Websites";
+$lang["add_website"] = "Add Website";
+$lang["client"] = "Company";
+$lang["clients"] = "Companies";
+$lang["import_clients"] = "Import Companies";
+$lang["add_client"] = "Add Company";
+$lang["client_groups"] = "Status";
+$lang["client_info"] = "Company Info";
+$lang["vault"] = "Vault";
+$lang["edit_vault"] = "Edit Vault Item";
+$lang["add_vault_item"] = "Add Item to Vault";
+$lang["vault_user"] = "Username";
+$lang["vault_pass"] = "Password";
+$lang["vault_pass_pin"] = "Encryption PIN";
+$lang["owner"] = "Agent";
+$lang["client_tasks"] = "Client Tasks";
+$lang["add_client_task"] = "Add Client Task";
+
+return $lang;
