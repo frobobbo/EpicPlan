df2e35020a7e1d845b7756a2a70b0ea12c4fdc90
Brett Johnson <johnsobd@gmail.com>
2021-10-13 16:18:55 -0400

    Debug Cron Job

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -17,13 +17,16 @@ class Cron extends App_Controller {
         ini_set('max_execution_time', 300); //execute maximum 300 seconds 
         //wait at least 5 minute befor starting new cron job
         $last_cron_job_time = get_setting('last_cron_job_time');
-
+        echo "last cron job time: " + $last_cron_job_time;
         $current_time = strtotime(get_current_utc_time());
 
         if ($last_cron_job_time == "" || ($current_time > ($last_cron_job_time * 1 + 300))) {
+            echo $last_cron_job_time * 1 + 300;
             $this->cron_job->run();
             app_hooks()->do_action("app_hook_after_cron_run");
             $this->Settings_model->save_setting("last_cron_job_time", $current_time);
+        } else {
+            echo "Don't Run Cron job";
         }
     }
 
